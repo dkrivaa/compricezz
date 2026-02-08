ec08e6f918847e1eaa044f94118bc1546b6ecb29
dkrivaa <dannykrivaa@hotmail.com>
2026-02-08 12:34:03 +0200

    Initial commit

--- ui/views/pricecomparison.py
+++ ui/views/pricecomparison.py
@@ -199,9 +199,9 @@ def render():
         for item in common:
             st.write(item)
             st.write(f"{item} - {next(d.get('ItemName') or d.get('ItemNm') for d in st.session_state[store_keys[1]] if d['ItemCode'] == item)}")
-            # for key in store_keys:
-            #     price = next(d['ItemPrice'] for d in st.session_state[key] if d['ItemCode'] == item)
-            #     st.write(f"{key}: ₪ {price:.2f}")
+            for key in store_keys:
+                price = next(d['ItemPrice'] for d in st.session_state[key] if d['ItemCode'] == item)
+                st.write(f"{key}: ₪ {price:.2f}")
             st.divider()
 
 
